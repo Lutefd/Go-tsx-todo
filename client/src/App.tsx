e4bd24f16be168008d6ec3d30df79001768d27ac
Luis Dourado <lfaddourado@gmail.com>
2022-12-26 13:55:00 -0300

    feat: add Todo interface for KeyedMutator

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,18 @@ import useSWR from 'swr'
 import {Box} from "@mantine/core";
 import AddTodo from "./components/AddTodo";
 
+export interface Todo{
+    id: number
+    titulo: string
+    descricao: string
+    feito: boolean
+}
+
 export const ENDPOINT = 'http://localhost:4000'
 
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((res) => res.json())
 function App() {
-    const {data, mutate} = useSWR('api/todos', fetcher)
+    const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
   return (
     <Box>
         <h1>{JSON.stringify(data)}</h1>
