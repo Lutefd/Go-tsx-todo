e00767d8aa35edcabf2708cf850ba81815e17d38
Luis Dourado <lfaddourado@gmail.com>
2022-12-26 14:23:12 -0300

    feat: add function to mark the tasks as done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import {Box, List, ThemeIcon} from "@mantine/core";
 import AddTodo from "./components/AddTodo";
 import {CheckCircleFillIcon} from "@primer/octicons-react";
 
-export interface Todo{
+export interface Todo {
     id: number
     titulo: string
     descrição: string
@@ -14,35 +14,49 @@ export interface Todo{
 export const ENDPOINT = 'http://localhost:4000'
 
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((res) => res.json())
+
 function App() {
     const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
-  return (
-    <Box sx={(theme) => ({
-        padding: '2rem',
-        width: '100%',
-        maxWidth: '40rem',
 
-    })
-    }>
-        <List spacing='xs' size='sm' mb={12} center>
-        {data?.map((todo) => {
-            return (
-                <List.Item key={`todo__${todo.id}`} icon={
-                    todo.feito ? (<ThemeIcon color='teal' size={24} radius='xl'>
-                        <CheckCircleFillIcon size={22}/>
-                    </ThemeIcon>) : (<ThemeIcon color='gray' size={24} radius='xl'>
-                        <CheckCircleFillIcon size={22}/>
-                    </ThemeIcon>)
-                }>
-                    <h2>{todo.titulo}</h2>
-                    <p>{todo.descrição}</p>
-                </List.Item>
-            )
-        })}
-        </List>
-        <AddTodo mutate={mutate}/>
-    </Box>
-  )
+    async function markTodoAsDone(id: number) {
+        const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+            method: 'PATCH',
+        }).then((res) => res.json())
+        mutate(updated)
+    }
+
+    return (
+        <Box sx={(theme) => ({
+            padding: '2rem',
+            width: '100%',
+            maxWidth: '40rem',
+
+        })
+        }>
+            <List spacing='xs' size='sm' mb={12} center>
+                {data?.map((todo) => {
+                    return (
+                        <List.Item
+                            key={`todo__${todo.id}`}
+                            icon={todo.feito ? (
+                                    <ThemeIcon color='teal' size={24} radius='xl'>
+                                        <CheckCircleFillIcon size={22}/>
+                                    </ThemeIcon>)
+                                : (<ThemeIcon color='gray' size={24} radius='xl'>
+                                    <CheckCircleFillIcon size={22}/>
+                                </ThemeIcon>)
+                            }
+                            onClick={() => markTodoAsDone(todo.id)}
+                        >
+                            <h2>{todo.titulo}</h2>
+                            <p>{todo.descrição}</p>
+                        </List.Item>
+                    )
+                })}
+            </List>
+            <AddTodo mutate={mutate}/>
+        </Box>
+    )
 }
 
 export default App
