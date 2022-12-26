9c1c70ef8fa3c33315e5a984a08c7223f71db3fa
Luis Dourado <lfaddourado@gmail.com>
2022-12-26 12:54:56 -0300

    feat: add post method

--- server/main.go
+++ server/main.go
@@ -6,13 +6,37 @@ import (
 	"log"
 )
 
+type Todo struct {
+	ID     int    `json:"id"`
+	Titulo string `json:"titulo"`
+	Feito  bool   `json:"feito"`
+	Body   string `json:"body"`
+}
+
 func main() {
 	fmt.Print("Hello World")
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("Server ok")
+	})
+
+	app.Post("api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
