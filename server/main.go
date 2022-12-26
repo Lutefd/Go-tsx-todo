3dca410147ffcb21960223c5124763b23ee32518
Luis Dourado <lfaddourado@gmail.com>
2022-12-26 13:16:18 -0300

    fix: fix cors error by adding cors config origins to all

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"log"
 )
 
@@ -17,6 +18,11 @@ func main() {
 	fmt.Print("Hello World")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
