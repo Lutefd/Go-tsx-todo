a436ddd67bfe10c48df07fad8a6d6f141ff25cda
Luis Dourado <lfaddourado@gmail.com>
2022-12-26 14:17:23 -0300

    feat: add checkbox icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
 import useSWR from 'swr'
-import {Box, List} from "@mantine/core";
+import {Box, List, ThemeIcon} from "@mantine/core";
 import AddTodo from "./components/AddTodo";
+import {CheckCircleFillIcon} from "@primer/octicons-react";
 
 export interface Todo{
     id: number
@@ -16,11 +17,26 @@ const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((res) => res.j
 function App() {
     const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher)
   return (
-    <Box>
+    <Box sx={(theme) => ({
+        padding: '2rem',
+        width: '100%',
+        maxWidth: '40rem',
+
+    })
+    }>
         <List spacing='xs' size='sm' mb={12} center>
         {data?.map((todo) => {
             return (
-                <List.Item key={`todo__${todo.id}`}><h2>{todo.titulo}</h2><br/><p>{todo.descrição}</p></List.Item>
+                <List.Item key={`todo__${todo.id}`} icon={
+                    todo.feito ? (<ThemeIcon color='teal' size={24} radius='xl'>
+                        <CheckCircleFillIcon size={22}/>
+                    </ThemeIcon>) : (<ThemeIcon color='gray' size={24} radius='xl'>
+                        <CheckCircleFillIcon size={22}/>
+                    </ThemeIcon>)
+                }>
+                    <h2>{todo.titulo}</h2>
+                    <p>{todo.descrição}</p>
+                </List.Item>
             )
         })}
         </List>
